0bbe43d21b258992d43502e2e4387cbe153ec905
Warren Cheetham <warren@cheetham.email>
2024-08-19 13:50:05 +1200

    Allow application to run in an iframe

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,24 @@ const nextConfig = {
   images: {
     domains: ["assets.headlesshost.com", "localhost"],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // or you can omit this header entirely
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://example.com;", // Adjust as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
